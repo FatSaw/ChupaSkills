3bb58030a216a8c8a92c106a1ffb8167b2ee5cb4
FatSaw <eklesosek@gmail.com>
2023-05-20 03:28:49 +0300

    Update SkillType.java

--- src/main/java/chupa/skills/chupaskils/SkillType.java
+++ src/main/java/chupa/skills/chupaskils/SkillType.java
@@ -3,7 +3,7 @@ package chupa.skills.chupaskils;
 import org.bukkit.Material;
 
 enum SkillType {
-HEALTH((byte)0,(byte)10,(byte)10,Material.TOTEM),DAMAGE((byte)0,(byte)10,(byte)11,Material.IRON_SWORD),SPEED((byte)0,(byte)10,(byte)12,Material.IRON_BOOTS),KRIT((byte)0,(byte)10,(byte)14,Material.REDSTONE),KRITCHANCE((byte)0,(byte)10,(byte)15,Material.BLAZE_POWDER),TAKEOVER((byte)0,(byte)10,(byte)16,Material.IRON_CHESTPLATE),MESSAGES((byte)1,(byte)1,(byte)-1,null);
+HEALTH((byte)0,(byte)10,(byte)10,Material.TOTEM_OF_UNDYING),DAMAGE((byte)0,(byte)10,(byte)11,Material.IRON_SWORD),SPEED((byte)0,(byte)10,(byte)12,Material.IRON_BOOTS),KRIT((byte)0,(byte)10,(byte)14,Material.REDSTONE),KRITCHANCE((byte)0,(byte)10,(byte)15,Material.BLAZE_POWDER),TAKEOVER((byte)0,(byte)10,(byte)16,Material.IRON_CHESTPLATE),MESSAGES((byte)1,(byte)1,(byte)-1,null);
 	protected final byte defaultvalue,maxlevel,menuplace;
 	protected final Material item;
 	private SkillType(byte defaultvalue,byte maxlevel,byte menuplace,Material item) {
